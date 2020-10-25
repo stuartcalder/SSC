998280d0fa67a8d4b9b04f747322349f001fef13
Stuart Calder <technoglub@protonmail.com>
2020-10-24 22:46:49 -0700

    Make argument processing more adaptable.

--- args.h
+++ args.h
@@ -1,6 +1,7 @@
 #ifndef SHIM_ARGS_H
 #define SHIM_ARGS_H
 #include <stdlib.h>
+#include <stdbool.h>
 #include "macros.h"
 /* Processing arguments... A series of C strings terminate by null. abc, def, ghi, NULL
  * 	How to handle some specific argument: a handler that can change the remaining arguments, and somehow change
@@ -9,11 +10,20 @@
 
 typedef void Shim_Arg_Handler_t (char **, int const, void * SHIM_RESTRICT);
 typedef Shim_Arg_Handler_t * Shim_Arg_Parser_t (char const *);
+typedef Shim_Arg_Parser_t * Shim_Arg_Processor_t (char const *, void * SHIM_RESTRICT);
+
+enum {
+	SHIM_ARGTYPE_NONE,
+	SHIM_ARGTYPE_SHORT,
+	SHIM_ARGTYPE_LONG
+};
+
+int SHIM_PUBLIC
+shim_argtype (char const *);
 
 void SHIM_PUBLIC
 shim_process_args (int argc, char ** argv,
-		   Shim_Arg_Parser_t * const short_parser,
-		   Shim_Arg_Parser_t * const long_parser,
-		   Shim_Arg_Parser_t * const floating_parser,
-		   void * SHIM_RESTRICT state_modifier);
+		   Shim_Arg_Processor_t * processor,
+		   void * SHIM_RESTRICT state);
+		   
 #endif /* ~ SHIM_ARGS_H */
--- impl/args.c
+++ impl/args.c
@@ -1,54 +1,35 @@
 #include "args.h"
 #include <stdbool.h>
 
-enum {
-	OP_TYPE_FLOATING_,
-	OP_TYPE_SHORT_,		/* i.e. -h */
-	OP_TYPE_LONG_		/* i.e. --help */
-};
-
-static int
-get_op_type_ (char const * str) {
-	char const * p = str;
-	int number_hyphens = 0;
-	while( *p++ == '-' )
-		++number_hyphens;
-	switch( number_hyphens ) {
+int SHIM_PUBLIC
+shim_argtype (char const * str) {
+	int num_hyphens = 0;
+	while( *str++ == '-' )
+		++num_hyphens;
+	switch( num_hyphens ) {
 		case 1:
-			return OP_TYPE_SHORT_;
+			return SHIM_ARGTYPE_SHORT;
 		case 2:
-			return OP_TYPE_LONG_;
+			return SHIM_ARGTYPE_LONG;
 		default:
-			return OP_TYPE_FLOATING_;
+			return SHIM_ARGTYPE_NONE;
 	}
 }
 
 void SHIM_PUBLIC
 shim_process_args (int argc, char ** argv,
-		   Shim_Arg_Parser_t * const short_parser,
-		   Shim_Arg_Parser_t * const long_parser,
-		   Shim_Arg_Parser_t * const floating_parser,
-		   void * SHIM_RESTRICT state_modifier)
+		   Shim_Arg_Processor_t * const processor,
+		   void * SHIM_RESTRICT state)
 {
 	for( int i = 1; i < argc; ++i ) {
-		char ** ptr = argv + i;
-		if( *ptr ) {
-			int const op_type = get_op_type_( *ptr );
-			Shim_Arg_Handler_t * handler;
-			switch( op_type ) {
-				case OP_TYPE_SHORT_:
-					handler = short_parser( *ptr );
-					break;
-				case OP_TYPE_LONG_:
-					handler = long_parser( *ptr );
-					break;
-				case OP_TYPE_FLOATING_:
-				default:
-					handler = floating_parser( *ptr );
-					break;
+		char ** p = argv + i;
+		if( *p ) {
+			Shim_Arg_Parser_t * parser = processor( *p, state );
+			if( parser ) {
+				Shim_Arg_Handler_t * handler = parser( *p );
+				if( handler )
+					handler( p, argc - i, state );
 			}
-			if( handler )
-				handler( ptr, argc - i, state_modifier );
 		}
 	}
 }
